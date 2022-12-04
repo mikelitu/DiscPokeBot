f8b08d799326a8f940b42168f96d0afe791ca611
mikelitu <mikelitubiomed@gmail.com>
2022-12-04 21:05:17 +0800

    solved printing bug

--- utils.py
+++ utils.py
@@ -5,49 +5,50 @@ import discord
 
 def write_message_stats(name, poke_df):
 
-    pokemon = get_poke_from_name(name, poke_df)
-    
-    if pokemon is None:
-        return "No hay pokemon con nombre {}".format(name)
+  pokemon = get_poke_from_name(name, poke_df)
 
-    name = pokemon['Name']
-    ps = pokemon['HP']
-    atack = pokemon['Attack']
-    defense = pokemon['Defense']
-    s_attack = pokemon['Sp. Atk']
-    s_defense = pokemon['Sp. Def']
-    vel = pokemon['Speed']
+  if pokemon is None:
+    return "No hay pokemon con nombre {}".format(name)
 
-    msg = (discord.Embed(title="STATS",
-                         description='```css\n{}\n```'.format(name),
-                         color=discord.Color.blurple())
-            .add_field(name="PS", value=ps)
-            .add_field(name="Ataque", value=atack)
-            .add_field(name="Defensa", value=defense)
-            .add_field(name="Ataque Especial", value=s_attack)
-            .add_field(name="Defensa Especial", value=s_defense)
-            .add_field(name="Velocidad", value=vel))
+  name = pokemon['Name'].to_string(index=False)
+  ps = int(pokemon['HP'])
+  atack = int(pokemon['Attack'])
+  defense = int(pokemon['Defense'])
+  s_attack = int(pokemon['Sp. Atk'])
+  s_defense = int(pokemon['Sp. Def'])
+  vel = int(pokemon['Speed'])
 
-    return msg
+  msg = (discord.Embed(title="{}".format(name),
+                       description='```css\nSTATS\n```',
+                       color=discord.Color.blurple())
+        .add_field(name="PS", value=ps)
+        .add_field(name="Ataque", value=atack)
+        .add_field(name="Defensa", value=defense)
+        .add_field(name="Ataque Especial", value=s_attack)
+        .add_field(name="Defensa Especial", value=s_defense)
+        .add_field(name="Velocidad", value=vel)
+        )
+
+  return msg
+
 
 def write_message_evs(name, poke_df):
 
-    pokemon = get_poke_from_name(name, poke_df)
-    
-    if pokemon is None:
-        return "No hay pokemon con nombre {}".format(name)
+  pokemon = get_poke_from_name(name, poke_df)
 
-    name = pokemon['Name']
-    ps = pokemon['HP EV']
-    atack = pokemon['Atk EV']
-    defense = pokemon['Def EV']
-    s_attack = pokemon['Sp. Atk EV']
-    s_defense = pokemon['Sp. Def EV']
-    vel = pokemon['Spe EV']
+  if pokemon is None:
+    return "No hay pokemon con nombre {}".format(name)
 
+  name = pokemon['Name'].to_string(index=False)
+  ps = int(pokemon['HP EV'])
+  atack = int(pokemon['Atk EV'])
+  defense = int(pokemon['Def EV'])
+  s_attack = int(pokemon['Sp. Atk EV'])
+  s_defense = int(pokemon['Sp. Def EV'])
+  vel = int(pokemon['Spe EV'])
 
-    msg = (discord.Embed(title="EV",
-                         description='```css\n{}\n```'.format(name),
+  msg = (discord.Embed(title="{}".format(name),
+                         description='```css\nEVs\n```',
                          color=discord.Color.blurple())
             .add_field(name="PS", value=ps)
             .add_field(name="Ataque", value=atack)
@@ -55,14 +56,14 @@ def write_message_evs(name, poke_df):
             .add_field(name="Ataque Especial", value=s_attack)
             .add_field(name="Defensa Especial", value=s_defense)
             .add_field(name="Velocidad", value=vel))
+  return msg
 
-    return msg
 
 def get_poke_from_name(name: str, poke_df: pd.DataFrame):
 
-    mask = poke_df["Name"].str.lower() == name.lower()
-    pos = np.flatnonzero(mask)
-    if len(pos) < 1:
-        return None
-    info = poke_df.iloc[pos]
-    return info
+  mask = poke_df["Name"].str.lower() == name.lower()
+  pos = np.flatnonzero(mask)
+  if len(pos) < 1:
+    return None
+  info = poke_df.iloc[pos]
+  return info
